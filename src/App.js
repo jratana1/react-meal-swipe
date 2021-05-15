11d8af327632dda232ed65f0725112aac4be1868
John Ratana <jratana1@gmail.com>
2021-05-14 22:07:20 -0400

    testing

--- src/App.js
+++ src/App.js
@@ -2,35 +2,62 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-      let config = {
-      method: 'GET',
-      headers: {
-          Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6NywiZXhwIjoxNjI1OTQxMDMxfQ.KjoSFea7NTjnq6NWqietCocQ5OnblTrdAmXSmey3ybA`
-      },
-      body: JSON.stringify(this.state)
-  }
-  fetch(`http://localhost:3000/users`, config)
-      .then(res => res.json())
-      .then(res => {
-          console.log(res)
-      })
+//  const handleClick = () => {
+//     fetch('http://localhost:3000/auth/facebook',{
+//     method: 'POST',
+    
+//     })
+//  } 
+
+// fetch('https://api.yelp.com/v3/graphql', {
+//   method: 'POST',
+//   headers: {
+//     'Content-Type': 'application/json',
+//     'Authorization': `Bearer w6qa8__4fH9T6fuiTpxA09hBfrKhosMvhe9N4EVtpZ6GaqJpTTasxnDkgApBCtGUGbiO9VinV1x4nU9VhVeMLepRZa1CZdHpK-o33NtvPj2LsFag44iGgPMqkx6eX3Yx`
+//   },
+//   body: JSON.stringify({
+//     query: `
+//         query GetRest{
+//   search(term: "burrito", location: "philly") {
+//     total
+//     business {
+//       name
+//       location {
+//         address1
+//       }
+//     }
+//   }
+// }`
+
+//   }),
+// })
+//   .then((res) => res.json())
+//   .then((result) => console.log(result));
+
 
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Login with google
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+<a href="http://localhost:3000/authenticate-facebook">Log-In with Google</a>
+
+<a href="http://localhost:3000/authenticate-facebook">here face</a>
+<a href="http://localhost:3000/authenticate-google">here google</a>
       </header>
+
+                <form
+                    id="main-login"
+                    action="http://localhost:3000/auth/google_oauth2"
+                    method="POST">
+                    <h2>
+                        Admin UI Login
+                    </h2>
+                        <button type="submit">Submit</button>
+
+                </form>
     </div>
   );
 }
@@ -50,3 +77,16 @@ export default App;
 //       .then(res => {
 //           console.log(res)
 //       })
+
+// let config = {
+  //     method: 'GET',
+  //     headers: {
+  //         Authorization: `Bearer eyJhbGciOiJIUzI1NiJ9.eyJpZCI6NywiZXhwIjoxNjI1OTQxMDMxfQ.KjoSFea7NTjnq6NWqietCocQ5OnblTrdAmXSmey3ybA`
+  //     },
+  //     body: JSON.stringify(this.state)
+  // }
+  // fetch(`http://localhost:3000/users`, config)
+  //     .then(res => res.json())
+  //     .then(res => {
+  //         console.log(res)
+  //     })
