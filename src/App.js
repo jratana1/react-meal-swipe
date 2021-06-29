ab916a4c839c9896931b76155b41b86f83ff73e1
John Ratana <jratana1@gmail.com>
2021-06-29 12:01:06 -0400

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,22 +1,61 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
 import './App.css';
+// import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter, Route, Switch } from 'react-router-dom';
+
+import Navbar from './components/Navbar'
+
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Login with google
-        </p>
-          <a href="http://localhost:3000/authenticate-facebook">Log-In with facebook</a>
-          <a href="http://localhost:3000/authenticate-google">Log-In with google</a>
-      </header>
-    </div>
-  );
+  const [isBusy, setBusy] = useState(true)
+
+  const renderLoad = () => {
+    if (isBusy) {
+      setBusy(false)
+      return <div>Loading</div>;
+    } else {
+      return (
+          <Navbar />
+        // <HashRouter basename='/'>
+        //   <Navbar />
+        //   <Switch>
+        //     <Route path='/' exact component={Home} />
+        //     <Route path='/swipe' exact component={About} />
+        //     <Route path='/profile' exact component={Contact} />
+    
+        //   </Switch>
+        // </HashRouter>
+      )
+    }
+  }
+
+    return (
+      <div className="App">
+            {renderLoad()}      
+      </div>
+    );
 }
 
+
 export default App;
+
+// return (
+//     <div className="App">
+//       <header className="App-header">
+//         <img src={logo} className="App-logo" alt="logo" />
+//         <p>
+//           Login with google
+//         </p>
+//           <a href="http://localhost:3000/authenticate-facebook">Log-In with facebook</a>
+//           <a href="http://localhost:3000/authenticate-google">Log-In with google</a>
+          
+//       </header>
+//     </div>
+//   );
+// }
+
+// export default App;
 
 
 // fetch('http://localhost:3000/users/sign_in', {
---
+++ src/components/Navbar.js
@@ -0,0 +1,37 @@
+import React, { useState } from 'react';
+
+import { makeStyles } from '@material-ui/core/styles';
+import BottomNavigation from '@material-ui/core/BottomNavigation';
+import BottomNavigationAction from '@material-ui/core/BottomNavigationAction';
+import RestoreIcon from '@material-ui/icons/Restore';
+import FavoriteIcon from '@material-ui/icons/Favorite';
+import LocationOnIcon from '@material-ui/icons/LocationOn';
+
+function Navbar() {
+  const [value, setValue] = React.useState(0);
+  const useStyles = makeStyles({
+    stickToBottom: {
+      width: '100%',
+      position: 'fixed',
+      bottom: 0,
+    },
+  });
+  const classes = useStyles();
+
+  return (
+    <BottomNavigation
+      value={value}
+      onChange={(event, newValue) => {
+        setValue(newValue);
+      }}
+      showLabels
+      className={classes.stickToBottom}
+    >
+      <BottomNavigationAction label="Recents" icon={<RestoreIcon />} />
+      <BottomNavigationAction label="Favorites" icon={<FavoriteIcon />} />
+      <BottomNavigationAction label="Nearby" icon={<LocationOnIcon />} />
+    </BottomNavigation>
+  );
+}
+
+export default Navbar
